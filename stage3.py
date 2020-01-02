851b819e4992ecfde6f4f97c1ca08949f3b259e6
SabriQ <196989054@qq.com>
2020-01-02 20:45:02 +0800

    update info.py

--- stage3.py
+++ stage3.py
@@ -1,7 +1,8 @@
 #from func_train import stage_3
 from func_test import stage_3
-from info import send_email
+from info import send_wechat_by_serverchan
 import sys
 if __name__ == "__main__":
+    send_wechat = send_wechat_by_serverchan()
     stage_3(serial_ports=[r'/dev/ttyUSB0',r'/dev/ttyUSB1'],mouse_id=sys.argv[1],note="test",video_record = False,according_to="Trial",Time=1200,Trial=60,data_dir=r"/home/qiushou/Documents/data/linear_track")
-    send_email("sqiu@ion.ac.cn","info from python",f"{sys.argv[1]} finish training")
+    send_wechat(f"{sys.argv[1]} finish training","nothing")
